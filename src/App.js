fe41ab0b6e68710e4627cebf331fe87bfc7f5db1
ekil <idgusqls0506@naver.com>
2022-06-21 00:33:46 +0900

    lecture 2, 3 practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let post = '부산대 고양이카페'; // 서버에서 가져온 정보라고 가정
+  let [title, b] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  // let [logo, setLogo] = useState('ReactBlog'); // state는 글자가 변경됐을 때 그 상태를 자동으로 반영해주는 역할, 사이트 로고는 자주 바뀌는 것이 아님 -> 굳이 state 써줄 필요 X
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React hehe
-        </a>
+      <header className="black-nav">
+        <h1>ReactBlog</h1>
       </header>
+      <ul>
+        <li className="list">
+          <h2>{title[0]}</h2>
+          <p>6월 20일 발행</p>
+        </li>
+        <li className="list">
+          <h2>{title[1]}</h2>
+          <p>6월 20일 발행</p>
+        </li>
+        <li className="list">
+          <h2>{title[2]}</h2>
+          <p>6월 20일 발행</p>
+        </li>
+      </ul>
+      <h2>{ post }</h2>
     </div>
   );
 }
